e91f41773a71f40c61f142e989b3b19eb351f0c5
ZhouYiDa <1983461670@qq.com>
2019-07-28 15:24:37 +0800

    add learn note

---
+++ firstMavenProject/src/main/java/Animal.java
@@ -0,0 +1,14 @@
+public class Animal{
+    private String name;
+    private int id;
+    public Animal(String myName,int myId){
+        name=myName;
+        id=myId;
+    }
+    public void eat(){
+        System.out.println(name+"正在吃");
+    }
+    public void sleep(){
+
+    }
+}
